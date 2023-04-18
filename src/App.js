b4c64e0f1db97520652e04982726ef24335585c9
thaytoanlaptrinh <thaytoanlaptrinh@gmail.com>
2023-04-18 23:07:39 +0700

    4 cai dat fontchu va reset css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
-import Index from '~/component/button';
 function App() {
     return (
         <>
-            <Index />
             <h1>Hello</h1>
         </>
     );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from '~/App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
+import GlobalStyles from './component/GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
+    // <React.StrictMode>
     <Router>
-      <App />
-    </Router>
-  // </React.StrictMode>
+        <GlobalStyles>
+            <App />
+        </GlobalStyles>
+    </Router>,
+    // </React.StrictMode>
 );
 
 reportWebVitals();
